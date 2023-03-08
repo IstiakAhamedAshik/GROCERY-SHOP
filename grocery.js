556ea046dabad66e1261fe5e5f9786bee4e3d182
Istiak Ahamed Ashik <istiak1439@gmail.com>
2023-03-08 22:17:58 +0600

    here is the js file of grocery

---
+++ grocery.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidePerGroup: 3,
+  centerSlide: 'true',
+  grabCursor: 'true',
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+  },
+})
+var swiper = new Swiper('.customer', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidePerGroup: 3,
+  centerSlide: 'true',
+  grabCursor: 'true',
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+  },
+})
